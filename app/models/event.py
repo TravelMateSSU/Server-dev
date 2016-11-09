98e24000b500893b39cccd9a37acaafe7eb93708
이준수 <ljs93kr@gmail.com>
2016-11-09 18:15:02 +0900

    event table 칼럼에 필요한 내용을 추가했습니다.
    - 모집글에 대한 내용을 포함합니다.

--- app/models/event.py
+++ app/models/event.py
@@ -7,7 +7,14 @@ class TravelEvent(db.Model):
     event_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True) # primary key for event table
     user_id = db.Column(db.String(30))          # 어느 사용자가 이벤트를 오픈했는지
     course_id = db.Column(db.Integer)           # 어느 코스를 사용하는지, 해당 코스의 detail 정보를 모두 가져올 수 있다
+    title = db.Column(db.String(30))            # 해당 모집글의 제목정보
     description = db.Column(db.String(1000))    # 사용자가 작성할 이벤트의 설명글
+    max_tourist = db.Column(db.Integer)         # 최대 모집자 수
+    current_tourist = db.Column(db.Integer)     # 현재 모집자 수
+    travel_start_time = db.Column(db.DateTime)  # 여행 시작 날짜와 시간
+    travel_end_time = db.Column(db.DateTime)    # 여행 끝 날짜와 시간
+    event_end_time = db.Column(db.DateTime)     # 모집이 끝나는 시간, 모집글이 등록되는 순간부터 모집 시작
+    status = db.Column(db.Integer)              # 모집여부, 0: 모집 끝, 1:모집 진행중
     hash_tag = db.Column(db.String(200))        # 검색을 위해서 이벤트가 지나가는 경로에 대한 해시태그
     created = db.Column(db.DateTime)
 
